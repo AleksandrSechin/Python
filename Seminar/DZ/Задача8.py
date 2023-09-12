cf00ef51846a4e688b820695a06ab4b4b33e6164
Alex <ansechin2012@ya.ru>
2023-09-12 21:44:30 +0300

    Деление шоколадки

--- Seminar/DZ/Задача8.py
+++ Seminar/DZ/Задача8.py
@@ -8,10 +8,10 @@
 a = 3
 b = 2
 c = 6
-# if c < a * b and c % a == 0 or c % b == 0:
-#     print('yes')
-# else: 
-#     print('no')
+if c < a * b and c % a == 0 or c % b == 0:
+    print('yes')
+else: 
+    print('no')
 
 
 
