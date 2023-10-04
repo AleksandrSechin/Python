df5e1fe81182ac1ad3b58d885527fd4e7c5cab9e
Alex <ansechin2012@ya.ru>
2023-10-04 14:27:26 +0300

    Функции и Модульность

--- Lessons/Функции.py
+++ Lessons/Функции.py
@@ -1,10 +1,19 @@
+import modul1
+import modul1 as m1
+from modul1 import max
+
+
 def sum_number(n):
     summa = 0
     for i in range(1, n+1):
         summa += i
     return summa
 
+
 print(sum_number(5))
+# или
+a = sum_number(5)
+print(a)
 
 
 # Передать неограниченное кол-во аргументов
@@ -15,16 +24,13 @@ def sum_str(*args):
         res += i
     return res
 
+
 print(sum_str('q', 'e', 'r'))
 print(sum_str('q', 'e', 'r', 't', 'y'))
 
 
 #  Модульность - вызов функции из другого файла
 
-import modul1               
-print(modul1.max1(5, 9))     
+print(modul1.max1(5, 9))
 
-from modul1 import max
-
-import modul1 as m1
 print(m1.max1(5, 9))
