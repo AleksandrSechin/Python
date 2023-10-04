443257401b42bd1d25a5083f210a33c14dd6bbd7
Alex <ansechin2012@ya.ru>
2023-10-04 14:53:53 +0300

    Функции и Модульность

--- Lessons/Функции.py
+++ Lessons/Функции.py
@@ -1,6 +1,14 @@
+#  Модульность - вызов функции из другого файла
+
 import modul1
 import modul1 as m1
-from modul1 import max
+from modul1 import *  # импорт всех функций модуля
+
+print(modul1.max1(5, 9))
+# или
+print(m1.max1(5, 9))
+
+# Функции
 
 
 def sum_number(n):
@@ -8,6 +16,7 @@ def sum_number(n):
     for i in range(1, n+1):
         summa += i
     return summa
+    print(stop)  # не выводится после return
 
 
 print(sum_number(5))
@@ -16,7 +25,13 @@ a = sum_number(5)
 print(a)
 
 
-# Передать неограниченное кол-во аргументов
+def max1(a, b):
+    if a > b:
+        return a  # return прерывает, else не используется
+    return b
+
+
+# Передать неограниченное кол-во аргументов (*args)
 
 def sum_str(*args):
     res = " "
@@ -27,10 +42,3 @@ def sum_str(*args):
 
 print(sum_str('q', 'e', 'r'))
 print(sum_str('q', 'e', 'r', 't', 'y'))
-
-
-#  Модульность - вызов функции из другого файла
-
-print(modul1.max1(5, 9))
-
-print(m1.max1(5, 9))
