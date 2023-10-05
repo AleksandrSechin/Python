d8f327a8df439411aec05732cc72c83076d8516d
Alex <ansechin2012@ya.ru>
2023-10-05 14:15:13 +0300

    Кол-во элементов которые больше соседних

---
+++ Seminar/Задача 41.py
@@ -0,0 +1,29 @@
+# Задача №41. Дан массив, состоящий из целых чисел. Напишите программу,
+# которая в данном массиве определит количество элементов, у которых два
+# соседних и, при этом, оба соседних элемента меньше данного. Сначала
+# вводится число N — количество элементов в массиве. Далее записаны N
+# чисел — элементы массива. Массив состоит из целых чисел
+# Ввод: 5       Ввод: 5
+# 1 2 3 4 5    1 5 1 5 1
+# Вывод: 0      Вывод: 2
+
+from random import randint
+
+
+def create_list(n):
+    new_list = [randint(0, 9) for _ in range(n)]
+    return new_list
+
+
+def count_elements(list1):
+    count = 0
+    for i in range(1, len(list1) - 1):
+        if list1[i - 1] < list1[i] > list1[i + 1]:
+            count += 1
+    return count
+
+
+n = randint(5, 10)
+list1 = create_list(n)
+print(n, ' -> ', list1)
+print(count_elements(list1))
